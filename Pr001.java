af03308017e13c38ac80600818b9c9f7971fb178
felger <wizardman14@mail.ru>
2025-09-02 12:15:52 +0700

    Первые программы java

---
+++ Pr001.java
@@ -0,0 +1,11 @@
+/*public*/ class A {
+	public static void a(String s){
+		System.out.println(s);
+	}
+}
+class Pr001 {
+	public static void main(String args[]){
+		A.a("Start of metod \"a()\"");
+		System.out.println("First programa na java");
+	}
+}
---
+++ Pr002.java
@@ -0,0 +1,5 @@
+class Pr002{
+	public static void main(String args[]){
+		A.a("Start of method a() from Pr002");
+	}
+}
