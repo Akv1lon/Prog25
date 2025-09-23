745714ae4f486741ab4a0dea81f49eb405da73f9
yura <wizardman140@gmail.com>
2025-09-23 11:11:35 +0700

    Добавили демонстрацию работы оператора switch()

--- Pr006.java
+++ Pr006.java
@@ -60,6 +60,52 @@ class Pr006 {
 					//Оставшиеся варианты выведут нижнее i
 					System.out.println("i не равно значениям от 1 до 4");
 			}
-	}
 
+	//Оператор switch
+	System.out.println("\nДемнострация работы оператора switch()");
+	for(i=0; i<10; i++)
+		switch(i){
+			case 0:
+				System.out.println("i=0");
+				break;
+			case 1:
+				System.out.println("i=1");
+				break;
+			case 2:
+				System.out.println("i=2");
+				break;
+			case 3:
+				System.out.println("i=3");
+				break;
+			case 4:
+				System.out.println("i=4");
+				break;
+			default:
+				System.out.println("i>=5");
+				break;
+		}
+	//Вложенные операторы switch
+	System.out.println();
+	ch ='R';
+	char ch1 ='A';
+	switch(ch){
+		case 'A': 
+			System.out.println("Значение A внешнего оператора switch");
+			switch(ch1){
+				case 'A':
+					System.out.println("Значение A внутреннего оператора switch");
+					break;
+			}
+		case 'R':
+			System.out.println("Значение R внешнего оператора switch");
+			switch(ch1){
+				case 'A':
+					System.out.println("Значение А внутреннего оператора switch");
+					break;
+			}
+			break;
+		default:
+				System.out.println("Значение по умолчанию для внешнего switch()");
+			}
+	}
 }
