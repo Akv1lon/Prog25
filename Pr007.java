acd197629a1c978ddf79cd0c274b8cccf8f978df
yurch <wizardman14@mail.ru>
2025-10-07 10:35:06 +0700

    Добавили описание оператора continue

--- Pr007.java
+++ Pr007.java
@@ -89,7 +89,25 @@ stop2:		{
 			}
 		}
 		System.out.println();
+		
+		//Оператор continue
+		for(i=0; i<=100; i++) {
+			if((i%2) != 0)
+				continue;
+			System.out.println(i + " ");
 
-
+		}
+		
+		//Применение continue с меткой
+outerloop:
+		for(i=1; i<10; i++){
+			System.out.print("\nВнешний цикл: проход " + i +", внутренний цикл: ");
+			for(int j=1; j<10; j++){
+				if(j==5)
+					continue outerloop;
+				System.out.print(j);
+			}
+		}
+		System.out.println();
 	}
 }
